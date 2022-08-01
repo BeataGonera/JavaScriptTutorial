2af0018bc271b98ca69c637f5b160ce113930281
Beata Gonera <beataiwonagonera@gmail.com>
2022-08-01 11:13:19 +0200

    #13 if statement

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-alert("Hello World!");
+var YouLikeMeat = false;
+var myNum = 10;
+
+if (myNum == 17){9
+
+    document.write("myNum is equal to 10");
+
+}else{
+
+    document.write("myNum is not equal to 10");
+}
