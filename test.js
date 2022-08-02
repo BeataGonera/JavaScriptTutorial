70b88afee26ae9f7ac879a8c55dcf8a122de2221
Beata Gonera <beataiwonagonera@gmail.com>
2022-08-02 10:10:44 +0200

    local and global variables

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
-for(i = 0; i < 10; ++i){
-
-  if (i === 5 || i === 3){
-    continue;
-   }
-   
-    console.log(i);
-
-    if (i === 7){
-
-        break;
-    }
+function getAverage (a,b) {
+    var average = (a+b)/2;
+    console.log(average);
+    return average;
 }
 
-console.log("I have broken outside the loop");
+var myResult = getAverage(7,12);
+function logResult(){
+    console.log("My result is " + myResult);
+}
+
+logResult();
