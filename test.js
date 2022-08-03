e4a7ee253cc93bbce9d12e1b43e8fca70f64d567
Beata Gonera <beataiwonagonera@gmail.com>
2022-08-03 11:42:23 +0200

    THIS keyword

--- test.js
+++ test.js
@@ -9,6 +9,20 @@ newCar.drive = function()
 
 newCar.drive();
 
-var newCar2 = {speed: 40, driver:"John", drive: function(){console.log("now driving really fast");}};
+var newCar2 = {
+    speed: 40,
+    driver:"John",
+    drive: function(speed, time){
+        console.log(speed * time);}, 
+    test: function(){
+        console.log(this);
+    }, 
+    logDriver: function(){
+        console.log("The driver's name is:" + this.driver);
+    }
 
-newCar2.drive();
+};
+
+newCar2.drive(50, 2);
+newCar2.test();
+newCar2.logDriver();
