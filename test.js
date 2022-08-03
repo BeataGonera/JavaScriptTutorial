62f3dcc248869adff42d4513836fddcc3beae66c
Beata Gonera <beataiwonagonera@gmail.com>
2022-08-03 10:35:26 +0200

    spliting strings

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-var string1 = "abc";
-var string2 = "ABC";
+var str="Hello, world.";
 
-console.log(string1.toLowerCase() === string2.toLowerCase());
+var str2= str.slice(2,9);
+
+console.log(str2);
+
+var tags = "pork, chicken, veggie, salami, banana";
+var tagsArray = tags.split(",");
+
+console.log(tagsArray);
